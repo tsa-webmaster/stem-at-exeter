6f93c6f3ea1213fe561dfd74861d720ad8478183
colsont <47868438+colsont@users.noreply.github.com>
2019-03-21 15:30:23 -0400

    Update stematexeter.js

--- stematexeter.js
+++ stematexeter.js
@@ -5,12 +5,10 @@ function openMenu() {
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
